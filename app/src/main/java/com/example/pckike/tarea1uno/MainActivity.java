08ee9365f3d572bb6c2c4f7681f841007ac481b1
kikecruzmamani <kikelike05@gmail.com>
2017-06-16 16:04:42 -0400

    rep

---
+++ app/src/main/java/com/example/pckike/tarea1uno/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.pckike.tarea1uno;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
